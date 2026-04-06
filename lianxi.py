ad472a29a62937a0793f03002bfea5648f4c3d59
rhys <zane1233@163.com>
2026-04-06 22:53:48 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -2599,5 +2599,20 @@ def jiema69():
     输出：0
     解释："06" 无法映射到 "F" ，因为存在前导零（"6" 和 "06" 并不等价）。
     """
+    s = input()
+    n = len(s)
+    if n == 0 or s[0] == '0':
+        print(0)
+        return
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n + 1):
+        if s[i-1] != '0':
+            dp[i] += dp[i-1]
+        two_digit = int(s[i-2:i])
+        if 10 <= two_digit <= 26:
+            dp[i] += dp[i-2]
+    print(dp[n])
 if __name__=='__main__':
-    ziji68()
+    jiema69()
