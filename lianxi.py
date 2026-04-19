8d5919f3b1723fee52520fea0e5b538c50d5d17b
rhys <zane1233@163.com>
2026-04-19 17:03:32 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -3057,7 +3057,80 @@ def weirao():
                 board[i][j] = 'X'
             elif board[i][j] == 'A':
                 board[i][j] = 'O'
-
     print(board)
+def jiayou75():
+    """
+    在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+    你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
+    给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。
+    如果存在解，则 保证 它是 唯一 的。
+
+    示例 1:
+    输入: gas = [1,2,3,4,5]; cost = [3,4,5,1,2]
+    输出: 3
+    解释:
+    从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+    开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+    开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+    开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+    开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+    开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+    因此，3 可为起始索引。
+
+    示例 2:
+    输入: gas = [2,3,4]; cost = [3,4,3]
+    输出: -1
+    解释:
+    你不能从 0 号或 1 号加油站出发，因为没有足够的汽油可以让你行驶到下一个加油站。
+    我们从 2 号加油站出发，可以获得 4 升汽油。 此时油箱有 = 0 + 4 = 4 升汽油
+    开往 0 号加油站，此时油箱有 4 - 3 + 2 = 3 升汽油
+    开往 1 号加油站，此时油箱有 3 - 3 + 3 = 3 升汽油
+    你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
+    因此，无论怎样，你都不可能绕环路行驶一周。
+    """
+    exec(input(), globals())
+    if sum(gas) < sum(cost):
+        return -1
+    y = 0;
+    zz = -1;
+    b = 0
+    i = 0
+    gasss = gas[:]
+    while (i < len(gas)):
+        if gas[i] == 0 and cost[i] == 0:
+            del gas[i]
+            del cost[i]
+            b += 1
+            continue
+        if gas[i] == 0 and i not in [0, 1, len(gas) - 1]:
+            del gas[i]
+            cost[i - 1] += cost[i]
+            del cost[i]
+            b += 1
+            continue
+        if cost[i] == 0 and i not in [0, 1, len(cost) - 1]:
+            del cost[i]
+            gas[i - 1] += gas[i]
+            del gas[i]
+            b += 1
+            continue
+        if gas[i] < cost[i]:
+            i += 1
+            continue
+        gass = gas[i:] + gas[:i];
+        costt = cost[i:] + cost[:i]
+        zz = i
+        for j in range(len(gass)):
+            y += gass[j]
+            y -= costt[j]
+            if y < 0:
+                y = 0;
+                zz = -1
+                break
+        if zz == i:
+            print(zz + b)
+            return
+        i += 1
+    print(-1)
 if __name__=='__main__':
-    weirao74()
+    jiayou75()
