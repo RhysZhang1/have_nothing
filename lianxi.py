9b0753a8dd27210b04e8a36770c27b0127ed9064
rhys <zane1233@163.com>
2026-05-26 20:05:36 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -5368,5 +5368,39 @@ def tiaoba111():
                 p[i]=p[i-1]+(1 if dp[i] else 0)
             return dp[-1]
         return two()
+def huihuan112():   #快慢针：找环
+    """
+    给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+    假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+    你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+
+    示例 1：
+    输入：nums = [1,3,4,2,2]
+    输出：2
+    示例 2：
+    输入：nums = [3,1,3,4,2]
+    输出：3
+    示例 3 :
+    输入：nums = [3,3,3,3,3]
+    输出：3
+
+    提示：
+    1 <= n <= 105
+    nums.length == n + 1
+    1 <= nums[i] <= n
+    nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次
+    """
+    def findDuplicate(nums: List[int]) -> int:
+        s=f=nums[0]
+        while True:
+            s=nums[s]
+            f=nums[nums[f]]
+            if s==f:
+                break
+        s=nums[0]
+        while s!=f:
+            s=nums[s]
+            f=nums[f]
+        return s
 if __name__=='__main__':
     zuida84()
