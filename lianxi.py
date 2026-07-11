c03c767418807e1e59c6b2c035ea217e28688ffa
rhys <zane1233@163.com>
2026-07-11 18:30:23 +0800

    Add files via upload

--- lianxi.py
+++ lianxi.py
@@ -6431,3 +6431,44 @@ def zuida124__():   #位掩码
                 else:
                     m=max(m,len(words[i])*len(words[j]))
         return m
+def kuaisumi130():
+    """
+    你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。
+
+    示例 1：
+    输入：a = 2, b = [3]
+    输出：8
+    示例 2：
+    输入：a = 2, b = [1,0]
+    输出：1024
+    示例 3：
+    输入：a = 1, b = [4,3,3,8,5,2]
+    输出：1
+    示例 4：
+    输入：a = 2147483647, b = [2,0,0]
+    输出：1198
+
+    提示：
+    1 <= a <= 231 - 1
+    1 <= b.length <= 2000
+    0 <= b[i] <= 9
+    b 不含前导 0
+    """
+    def superPow(a, b):
+        # z=1
+        # for i in b:
+        #     z=(z**10)%1337
+        #     z=z*(a**i)%1337
+        # return z
+
+        Q=(7-1)*(191-1)
+        m=0
+        for i in b:
+            m=(m*10+i)%Q
+        m+=Q
+        b_=int(''.join(map(str,b[-4:])))
+        if len(b)<=4 and b_<Q:
+            z=(a**b_)%1337
+        else:
+            z=(a**m)%1337
+        return z
